fca40e6ab5fedd1ae9de7b47235baa559d25628b
Changqing Jing <changqing.jing@bmw.com>
2021-01-03 19:07:53 +0800

    remove bifield

--- genMachineCode/src/genMachineCode.cpp
+++ genMachineCode/src/genMachineCode.cpp
@@ -4,52 +4,53 @@
 #include <cstring>
 #include "Arrch64Register.h"
 
-struct __attribute__((packed))  MoveWideImmediate{
-    static const uint16_t OPCODEDEF = 0b100101;
+namespace  MoveWideImmediate{
     enum SF{
         bit32=0,
-        bit64=1,
+        bit64=0b10000000'00000000'00000000'00000000,
     };
     enum OPC{
-        MOVN = 0b00,
-        MOVZ = 0b10,
-        MOVK = 0b11,
+        MOVN = 0,
+        MOVZ = 0b10'00000000'00000000'00000000'00000,
+        MOVK = 0b11'00000000'00000000'00000000'00000,
     };
     
-    Aarch64Register Rd:5;
-    uint16_t imm16:16;
-    uint8_t hw:2;
-    uint16_t opCode:6;
-    OPC opc:2;
-    SF sf:1;
 
-    static inline void emmitMoveImmediateValue(Aarch64Register reg, uint16_t imm16, uint8_t hw, OPC opc, SF sf, MoveWideImmediate* instruction){
-        instruction->Rd = reg;
-        instruction->imm16 = imm16;
-        instruction->hw = hw;
-        instruction->opCode = OPCODEDEF;
-        instruction->opc = opc;
-        instruction->sf = sf;
+    
+    const uint8_t registerMask = 0b1'1111;
+    const uint32_t imm16Mask = 0b11111111'11111111'00000;
+    const uint8_t imm16Offset = 5;
+    const uint8_t hwOffset = imm16Offset + sizeof(uint16_t)*8;
+    const uint32_t opCode = 0b100101'00'00000000'00000000'00000;
+
+    static inline void emmitMoveImmediateValue(Aarch64Register reg, uint16_t imm16, uint8_t hwBase, OPC opc, SF sf, uint32_t* instruction){
+        *instruction = opCode;
+        *instruction |= (reg&registerMask);
+        *instruction |= ((imm16<<imm16Offset)&imm16Mask);
+        *instruction |= (hwBase<<hwOffset);
+        //*instruction |= opCode; set opcode is not necessary here, keep as comment for better readability
+        *instruction |= opc;
+        *instruction |= sf;
     }
 };
 
 
 
 int emmitMoveImmediateValue64(Aarch64Register reg, uint64_t value, uint32_t*& mc){
-    MoveWideImmediate* mov_ins = (MoveWideImmediate*)mc;
+
     if((~value)<=UINT16_MAX){
-        MoveWideImmediate::emmitMoveImmediateValue(reg, ~(value), 0, MoveWideImmediate::OPC::MOVN,  MoveWideImmediate::SF::bit64, mov_ins);
+        MoveWideImmediate::emmitMoveImmediateValue(reg, ~(value), 0, MoveWideImmediate::OPC::MOVN,  MoveWideImmediate::SF::bit64, mc);
         mc++;
         return 0;
     }
     
-    MoveWideImmediate::emmitMoveImmediateValue(reg, value&UINT16_MAX, 0, MoveWideImmediate::OPC::MOVZ,  MoveWideImmediate::SF::bit64, mov_ins);
+    MoveWideImmediate::emmitMoveImmediateValue(reg, value&UINT16_MAX, 0, MoveWideImmediate::OPC::MOVZ,  MoveWideImmediate::SF::bit64, mc);
     mc++;
     uint8_t hw = 1;
 
     while((value = value>>16)>0){
-        MoveWideImmediate* mov_ins = (MoveWideImmediate*)mc;
-        MoveWideImmediate::emmitMoveImmediateValue(reg, value&UINT16_MAX, hw, MoveWideImmediate::OPC::MOVK,  MoveWideImmediate::SF::bit64, mov_ins);
+
+        MoveWideImmediate::emmitMoveImmediateValue(reg, value&UINT16_MAX, hw, MoveWideImmediate::OPC::MOVK,  MoveWideImmediate::SF::bit64, mc);
         hw++;
         mc++;
     }
